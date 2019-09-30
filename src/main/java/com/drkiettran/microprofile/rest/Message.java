bdce49323a1522e9bce68dc93e6fffe039b87819
Tejas <TTipnis@my.harrisburgu.edu>
2019-09-29 21:43:32 -0400

    changed code to add first and last name

--- src/main/java/com/drkiettran/microprofile/rest/Message.java
+++ src/main/java/com/drkiettran/microprofile/rest/Message.java
@@ -19,6 +19,39 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 public class Message implements Serializable {
 	private static final long serialVersionUID = 2120914098830910225L;
 
+	@JsonProperty("id")
+	private int id;
+	
+	@JsonProperty("first-name")
+	private String firstName;
+	
+	@JsonProperty("last-name")
+	private String lastName;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
 	@JsonProperty("name")
 	private String name;
 
